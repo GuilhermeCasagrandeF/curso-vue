a30db5ed4f2b17f03693223367ed8b36731c8e48
GuilhermeCasagrandeF <118361478+GuilhermeCasagrandeF@users.noreply.github.com>
2022-12-28 20:07:17 -0300

    Trazendo as tarefas da api

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,13 +1,15 @@
 import IProjeto from "@/interfaces/IProjeto";
+import ITarefa from "@/interfaces/ITarefa";
 import { createStore, Store, useStore as vuexUseStore } from "vuex";
 import { InjectionKey } from 'vue'
-import { ADICIONA_PROJETO, ALTERA_PROJETO, DEFINIR_PROJETOS, EXCLUIR_PROJETO, NOTIFICAR } from "./tipo-mutacoes";
+import { ADICIONA_PROJETO, ALTERA_PROJETO, DEFINIR_PROJETOS, DEFINIR_TAREFAS, EXCLUIR_PROJETO, NOTIFICAR } from "./tipo-mutacoes";
 import { INotificacao } from "@/interfaces/INotificacao";
-import { ALTERAR_PROJETO, CADASTRAR_PROJETO, OBTER_PROJETOS, REMOVER_PROJETO } from "./tipo-acoes";
+import { ALTERAR_PROJETO, CADASTRAR_PROJETO, OBTER_PROJETOS, OBTER_TAREFAS, REMOVER_PROJETO } from "./tipo-acoes";
 import clienteHttp from "../http";
 
 interface Estado {
     projetos: IProjeto[],
+    tarefas: ITarefa[],
     notificacoes: INotificacao[]
 }
 
@@ -16,6 +18,7 @@ export const key: InjectionKey<Store<Estado>> = Symbol()
 export const store = createStore<Estado>({
     state: {
         projetos: [],
+        tarefas: [],
         notificacoes: []
     },
     mutations: {
@@ -35,6 +38,9 @@ export const store = createStore<Estado>({
         },
         [DEFINIR_PROJETOS](state, projetos: IProjeto[]) {
             state.projetos = projetos;
+        },
+        [DEFINIR_TAREFAS](state, tarefas: ITarefa[]) {
+            state.tarefas = tarefas;
         },
         [NOTIFICAR] (state, novaNotificacao: INotificacao) {
 
@@ -62,6 +68,10 @@ export const store = createStore<Estado>({
         [REMOVER_PROJETO] ({commit}, id: string) {
             return clienteHttp.delete(`/projetos/${id}`)
             .then(() => commit(EXCLUIR_PROJETO, id));
+        },
+        [OBTER_TAREFAS] ({ commit }) {
+            clienteHttp.get('tarefas')
+            .then(resposta => commit(DEFINIR_TAREFAS, resposta.data));
         },
 
     }
