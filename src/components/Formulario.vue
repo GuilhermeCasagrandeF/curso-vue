61cb6579433c9635f99952ece0b3bdd9e35a9718
GuilhermeCasagrandeF <118361478+GuilhermeCasagrandeF@users.noreply.github.com>
2023-05-30 20:30:14 -0300

    Refatorado componente para composition api contendo emit

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -31,7 +31,7 @@
 </template>
 
 <script lang="ts">
-import { computed, defineComponent } from "vue";
+import {computed, defineComponent, ref} from "vue";
 import Temporizador from "./Temporizador.vue";
 import { useStore } from 'vuex'
 
@@ -43,26 +43,27 @@ export default defineComponent({
   components: {
     Temporizador,
   },
-  data () { 
-    return {
-      descricao: '',
-      idProjeto: ''      
-    }
-  },
-  methods: {
-    salvarTarefa (tempoEmSegundos: number) : void {    
-      this.$emit('aoSalvarTarefa', {
-        duracaoEmSegundos: tempoEmSegundos,
-        descricao: this.descricao,
-        projeto: this.projetos.find(proj => proj.id == this.idProjeto)
-      })
-      this.descricao = ''
-    }
-  },
-  setup () {
+
+  setup (props, contexto) {
     const store = useStore(key)
+    const descricao = ref('');
+    const idProjeto = ref('');
+    const projetos = computed(() => store.state.projeto.projetos);
+
+    const salvarTarefa = (tempoEmSegundos: number) : void => {
+      contexto.emit('aoSalvarTarefa', {
+        duracaoEmSegundos: tempoEmSegundos,
+        descricao: descricao.value,
+        projeto: projetos.value.find(proj => proj.id == idProjeto.value)
+      })
+      descricao.value = ''
+    }
+
     return {
-      projetos: computed(() => store.state.projeto.projetos)
+      projetos,
+      descricao,
+      idProjeto,
+      salvarTarefa,
     }
   }
 });
