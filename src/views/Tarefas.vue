ffd4eeea950c5b2e157fb779b4a701f0927cd76f
GuilhermeCasagrandeF <118361478+GuilhermeCasagrandeF@users.noreply.github.com>
2023-05-31 18:42:16 -0300

    Modal de tarefas separado em um novo componente

--- src/views/Tarefas.vue
+++ src/views/Tarefas.vue
@@ -21,34 +21,37 @@
       @aoTarefaClicada="selecionarTarefa"
     />
 
-    <div class="modal" :class="{ 'is-active': tarefaSelecionada }" v-if="tarefaSelecionada">
-      <div class="modal-background"></div>
-      <div class="modal-card">
-        <header class="modal-card-head">
-          <p class="modal-card-title">Editar Tarefa</p>
-          <button
-            class="delete"
-            aria-label="close"
-            @click="fecharModal"
-          ></button>
-        </header>
-        <section class="modal-card-body">
-          <div class="field">
-            <label for="descricaoDaTarefa" class="label"> Descrição </label>
-            <input
-              type="text"
-              class="input"
-              v-model="tarefaSelecionada.descricao"
-              id="descricaoDaTarefa"
-            />
-          </div>
-        </section>
-        <footer class="modal-card-foot">
-          <button class="button is-success" @click="alterarTarefa">Salvar Alterações</button>
-          <button class="button" @click="fecharModal">Cancelar</button>
-        </footer>
-      </div>
-    </div>
+    <Modal :mostrar="tarefaSelecionada != null">
+      <header class="modal-card-head">
+        <p class="modal-card-title">Editar Tarefa</p>
+        <button
+          class="delete"
+          aria-label="close"
+          @click="fecharModal"
+        ></button>
+      </header>
+      <section class="modal-card-body">
+        <div class="field">
+          <label for="descricaoDaTarefa"
+                 class="label"> Descrição </label>
+          <input
+            type="text"
+            class="input"
+            v-model="tarefaSelecionada.descricao"
+            id="descricaoDaTarefa"
+          />
+        </div>
+      </section>
+      <footer class="modal-card-foot">
+        <button class="button is-success"
+                @click="alterarTarefa">Salvar Alterações
+        </button>
+        <button class="button"
+                @click="fecharModal">Cancelar
+        </button>
+      </footer>
+    </Modal>
+
   </div>
 </template>
 
@@ -65,10 +68,12 @@ import {
   OBTER_TAREFAS,
 } from "@/store/tipo-acoes";
 import ITarefa from "@/interfaces/ITarefa";
+import Modal from "@/components/Modal.vue";
 
 export default defineComponent({
   name: "App",
   components: {
+    Modal,
     Formulario,
     Tarefa,
     Box,
